17bbb228294d5fe2d6cbb71ca8c95f3873f21482
unknown <darathi.backiyaraj@innovationfactory.biz>
2023-11-13 15:42:15 +0400

    Initializing

---
+++ src/Java/Login.java
@@ -0,0 +1,21 @@
+package Java;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class Login {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C://Users//user//eclipse-workspace//chromedriver-win64");
+     
+	  WebDriver d= new ChromeDriver();  
+      
+      d.get("https://www.makemytrip.com/");  //URL
+      
+    
+}
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module SeleniumWebdriver {
+	requires org.seleniumhq.selenium.chrome_driver;
+	requires org.seleniumhq.selenium.api;
+}
